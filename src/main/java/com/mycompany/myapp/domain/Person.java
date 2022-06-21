5b459d9cc0cd04a55a42eb34721162457da9d0f8
quangvuminh2000 <quang.vuminh@hcmut.edu.vn>
2022-06-21 16:12:52 +0700

    Create a simple Person Entity

---
+++ src/main/java/com/mycompany/myapp/domain/Person.java
@@ -0,0 +1,75 @@
+package com.mycompany.myapp.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/*
+* A Person
+* */
+@Entity
+@Table(name = "jhi Person")
+public class Person implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Size(max = 100)
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Person)) {
+            return false;
+        }
+        return id != null && id.equals(((Person) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "User{" +
+            "name='" + name + '\'' +
+            ", age='" + age + '\'' +
+            "}";
+    }
+}
