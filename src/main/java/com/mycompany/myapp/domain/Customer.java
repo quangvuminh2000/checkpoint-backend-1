5ebdff2008c4eac823b13e2e99ebd6c2c17aeaff
quangvuminh2000 <quang.vuminh@hcmut.edu.vn>
2022-06-23 14:28:06 +0700

    Instantiate the Customer Entity model

---
+++ src/main/java/com/mycompany/myapp/domain/Customer.java
@@ -0,0 +1,84 @@
+package com.mycompany.myapp.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/*
+ * A class to store Customer information
+ * */
+@Entity
+@Table(name = "jhi_customer")
+public class Customer extends AbstractAuditingEntity implements Serializable {
+    @Id
+    @SequenceGenerator(sequenceName = "customer_sequence", name = "customer_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_sequence")
+    private Long id;
+
+    @Size(max = 100)
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "dob")
+    private LocalDate dob;
+
+    @Size(max = 100)
+    @Column(name = "city", length = 100)
+    private String city;
+
+    @Transient
+    @Column(name = "age")
+    private Integer age;
+
+    public Customer(String name, LocalDate dob, String city) {
+        this.name = name;
+        this.dob = dob;
+        this.city = city;
+    }
+
+    public Customer() { }
+
+    /* Getter */
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    /* Setter */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
