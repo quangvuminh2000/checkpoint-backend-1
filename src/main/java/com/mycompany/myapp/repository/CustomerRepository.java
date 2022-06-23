39a22b55d4f42f866f06779cf9676bb8fbd110ee
quangvuminh2000 <quang.vuminh@hcmut.edu.vn>
2022-06-23 15:42:09 +0700

    Add API to get all customers with the same name, dob or city

--- src/main/java/com/mycompany/myapp/repository/CustomerRepository.java
+++ src/main/java/com/mycompany/myapp/repository/CustomerRepository.java
@@ -2,7 +2,24 @@ package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> { }
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query("SELECT c FROM Customer c WHERE c.name = :name AND c.dob = :dob")
+    Optional<Customer> findByNameAndDob(String name, LocalDate dob);
+
+    @Query("SELECT c FROM Customer c WHERE c.name = :name")
+    List<Customer> findAllByName(String name);
+
+    @Query("SELECT c FROM Customer c WHERE c.dob = :dob")
+    List<Customer> findAllByDob(LocalDate dob);
+
+    @Query("SELECT c FROM Customer c WHERE c.city = :city")
+    List<Customer> findAllByCity(String city);
+}
