051f84f80f425cba4875a11c36c91834c57f3ccc
quangvuminh2000 <quang.vuminh@hcmut.edu.vn>
2022-06-23 14:21:22 +0700

    Add useful CRUD methods - find & delete to extend Spring Boot default operations

--- src/main/java/com/mycompany/myapp/repository/PersonRepository.java
+++ src/main/java/com/mycompany/myapp/repository/PersonRepository.java
@@ -2,7 +2,23 @@ package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {}
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    // SELECT p FROM jhi_person p WHERE p.name=? AND p.age=?
+    @Query("SELECT p FROM Person p WHERE p.name = ?1 AND p.age = ?2")
+    Optional<Person> findPersonByNameAndAge(String name, Integer age);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Person p WHERE p.name = :name AND p.age = :age")
+    void deleteByNameAndAge(@Param("name") String name, @Param("age") Integer age);
+}
