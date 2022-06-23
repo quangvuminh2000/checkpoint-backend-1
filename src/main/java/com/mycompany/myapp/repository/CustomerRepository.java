1509adc6b0b3da7759ad839a07ada941cff7f665
quangvuminh2000 <quang.vuminh@hcmut.edu.vn>
2022-06-23 16:13:55 +0700

    Add Delete to DB query to delete customer found by name and dob

--- src/main/java/com/mycompany/myapp/repository/CustomerRepository.java
+++ src/main/java/com/mycompany/myapp/repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -22,4 +24,9 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     @Query("SELECT c FROM Customer c WHERE c.city = :city")
     List<Customer> findAllByCity(String city);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Customer c WHERE c.name = :name AND c.dob = :dob")
+    void deleteByNameAndDob(String name, LocalDate dob);
 }
