bc798a368456737c854fb885c2b7707317023140
quangvuminh2000 <quang.vuminh@hcmut.edu.vn>
2022-06-23 16:15:13 +0700

    Add Delete Service to call for Delete Customer in the database

--- src/main/java/com/mycompany/myapp/service/CustomerService.java
+++ src/main/java/com/mycompany/myapp/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.mycompany.myapp.service;
 
 import com.mycompany.myapp.domain.Customer;
 import com.mycompany.myapp.repository.CustomerRepository;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -43,6 +44,24 @@ public class CustomerService {
     public List<Customer> getSameCity(String city) {
         return customerRepository.findAllByCity(city);
     }
+
+    public void deleteFirstFoundByNameAndDob(String name, String dob_string) {
+        LocalDate dob = this.StringToLocalDate(dob_string);
+
+        Optional<Customer> customerOptional = customerRepository.findByNameAndDob(name, dob);
+
+        if (customerOptional.isPresent()) {
+            try {
+                customerRepository.deleteByNameAndDob(name, dob);
+            } catch (InvalidDataAccessApiUsageException queryError) {
+                System.out.println("Error appear missing transaction in deletion");
+                System.err.println(queryError.getMessage());
+            }
+        } else {
+            throw new RuntimeException(String.format("Cannot find exact match person named %s born in %s in DB", name, dob_string));
+        }
+    }
+
     private LocalDate StringToLocalDate(String string) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate localDate = LocalDate.parse(string, formatter);
